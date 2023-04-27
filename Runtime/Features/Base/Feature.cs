0b62b69ba9d89aeea461f1b9feb9a3828e02f057
eterlan <703246769@qq.com>
2023-04-27 14:07:35 +0800

    1

--- Runtime/Features/Base/Feature.cs
+++ Runtime/Features/Base/Feature.cs
@@ -108,4 +108,51 @@ namespace Lib
             v3 = owner.GetOrAddValue<T3>();
         }
     }
+    
+        
+    [Serializable]
+    public abstract class Feature<T1, T2, T3, T4> : Feature where T1 : IValue, new() 
+                                                        where T2 : IValue, new()
+                                                        where T3 : IValue, new()
+                                                        where T4 : IValue, new()
+    {
+        protected T1 v1;
+        protected T2 v2;
+        protected T3 v3;
+        protected T4 v4;
+
+        protected internal override void OnEnabled(FeatureContainer owner)
+        {
+            base.OnEnabled(owner);
+            v1 = owner.GetOrAddValue<T1>();
+            v2 = owner.GetOrAddValue<T2>();
+            v3 = owner.GetOrAddValue<T3>();
+            v4 = owner.GetOrAddValue<T4>();
+        }
+    }
+    
+            
+    [Serializable]
+    public abstract class Feature<T1, T2, T3, T4, T5> : Feature where T1 : IValue, new() 
+                                                            where T2 : IValue, new()
+                                                            where T3 : IValue, new()
+                                                            where T4 : IValue, new()
+                                                            where T5 : IValue, new()
+    {
+        protected T1 v1;
+        protected T2 v2;
+        protected T3 v3;
+        protected T4 v4;
+        protected T5 v5;
+
+        protected internal override void OnEnabled(FeatureContainer owner)
+        {
+            base.OnEnabled(owner);
+            v1 = owner.GetOrAddValue<T1>();
+            v2 = owner.GetOrAddValue<T2>();
+            v3 = owner.GetOrAddValue<T3>();
+            v4 = owner.GetOrAddValue<T4>();
+            v5 = owner.GetOrAddValue<T5>();
+        }
+    }
 }
